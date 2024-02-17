514185fc33721881f6c3c09db8e56531a36e7693
Robin Bellenguer <robin.bellenguer@gmail.com>
2024-02-17 14:34:01 +0100

    Filtre dynamique ok + ajout design page login.html

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -23,23 +23,32 @@ afficherTravaux();
 function filtreTravaux(value) {
 }
 
-/* Affichage galerie avec filtres */
-window.onload = () => {
-    let filtres = document.querySelectorAll("#filtres button");
+/* Affichage de la galerie avec les filtres */
+let filtres = document.querySelectorAll("#filtres button");
 
-    for (let filtre of filtres) {
-        filtre.addEventListener("click", function () {
-            let etiquette = this.id.split("-")[1]; // Je récupère l'identifiant de catégorie à partir du bouton filtre
+for (let filtre of filtres) {
+    filtre.addEventListener("click", function () {
+        let etiquette = this.id.split("-")[1]; // Je récupère l'identifiant de catégorie à partir du bouton filtre
 
-            let figures = document.querySelectorAll("#gallery figure"); // Je récupère toutes les figures de la galerie
+        let figures = document.querySelectorAll("#gallery figure"); // Je récupère toutes les figures de la galerie
 
-            for (let figure of figures) { // Pour chaque figure, je n'active que celle qui correspond à la bonne étiquette
-                figure.classList.replace("active", "inactive"); // Les figures sont par défaut inactives
+        for (let figure of figures) { // Pour chaque figure, je n'active que celle qui correspond à la bonne étiquette
+            figure.classList.replace("active", "inactive"); // Les figures sont par défaut inactives
                 
-                if (!etiquette || figure.dataset.category === etiquette) { // Si aucune étiquette sélectionnée ou si la figure correspond à la catégorie sélectionnée
+            if (!etiquette || figure.dataset.category === etiquette) { // Si aucune étiquette sélectionnée ou si la figure correspond à la catégorie sélectionnée
                     figure.classList.replace("inactive", "active"); // J'active la figure
-                }
-            }
-        });
-    }
-};
+             }
+        }
+    });
+}
+
+/* Affichage dynamique du bouton filtre qd la catégorie est sélectionnée */
+let btnFiltres = document.querySelectorAll(".btn-filtre");
+for (let btnFiltre of btnFiltres) {
+    btnFiltre.addEventListener("click", function () {
+        for (let otherBtn of btnFiltres) {
+            otherBtn.classList.remove("active");
+        }
+      this.classList.add("active");
+    });
+}
