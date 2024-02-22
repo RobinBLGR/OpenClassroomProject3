e8cf308e78db7fac653f0d6b2168fc69caa0dada
Robin Bellenguer <robin.bellenguer@gmail.com>
2024-02-22 17:15:55 +0100

    Correction fct afficherTravaux + ajout galerie dans la modale

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,90 +1,95 @@
 /* Affichage dynamique de la galerie - function afficherTravaux */
-const galerie = document.getElementById("gallery");
+async function afficherTravaux() {
+    try {
+        const response = await fetch('http://localhost:5678/api/works');
+        const data = await response.json();
+        const gallery = document.getElementById('gallery');
 
-const afficherTravaux = () => {
-    fetch("http://localhost:5678/api/works")
-        .then(function (resp) {
-            return resp.json();
-        })
-        .then(function (data) {
-            for (const work in data) {
-                galerie.innerHTML += 
-                `<figure data-category="${data[work].categoryId}" class="active">
-                    <img src="${data[work].imageUrl}" alt="${data[work].title}">
-                    <figcaption>${data[work].title}</figcaption>
-                </figure>`;
-            }
+        data.forEach(photo => {
+            const container = document.createElement('div');
+            container.classList.add('photo-container');
+            container.dataset.category = photo.categoryId;
+
+            const img = document.createElement('img');
+            img.src = photo.imageUrl;
+            img.alt = photo.title;
+            img.classList.add('active');
+
+            const title = document.createElement('p');
+            title.textContent = photo.title;
+
+            container.appendChild(img);
+            container.appendChild(title);
+
+            gallery.appendChild(container);
         });
-};
-
-afficherTravaux();
+    } catch (error) {
+        console.error('Erreur lors du chargement des photos', error);
+    }
+}
 
 function filtreTravaux() {
-    let etiquette = this.id.split("-")[1]; // Je récupère l'identifiant de catégorie à partir du bouton filtre
-        
-    let figures = document.querySelectorAll("#gallery figure"); // Je récupère toutes les figures de la galerie
-        
-    for (let figure of figures) { // Pour chaque figure, je n'active que celle qui correspond à la bonne étiquette
-        figure.classList.replace("active", "inactive"); // Les figures sont par défaut inactives
-                        
-        if (!etiquette || figure.dataset.category === etiquette) { // Si aucune étiquette sélectionnée ou si la figure correspond à la catégorie sélectionnée
-            figure.classList.replace("inactive", "active"); // J'active la figure
+    const etiquette = this.id.split("-")[1];
+    const filters = document.querySelectorAll(".btn-filtre"); // Sélectionnez tous les boutons filtres
+
+    // Supprimez la classe "active" de tous les boutons filtres
+    filters.forEach(filter => {
+        filter.classList.remove("active");
+    });
+
+    // Ajoutez la classe "active" uniquement au bouton cliqué
+    this.classList.add("active");
+
+    const containers = document.querySelectorAll("#gallery .photo-container");
+
+    for (const container of containers) {
+        if (!etiquette || etiquette === "0" || container.dataset.category === etiquette) {
+            container.classList.add("active");
+            container.classList.remove("inactive");
+        } else {
+            container.classList.remove("active");
+            container.classList.add("inactive");
         }
     }
 }
 
-/* Affichage de la galerie avec les filtres + dynamique du bouton filtre qd la catégorie est sélectionnée */
-const filters = document.getElementById("filtres")
-fetch ("http://localhost:5678/api/categories")
-    .then (response => response.json())
-    .then (categories => {
+const filters = document.getElementById("filtres");
+fetch("http://localhost:5678/api/categories")
+    .then(response => response.json())
+    .then(categories => {
         categories.unshift({
             id: 0,
             name: "Tous"
-        })
+        });
         for (const category of categories) {
-            const filter = document.createElement("button")
-            filter.innerText = category.name 
-            filter.id = `categorie-${category.id}`
-            filter.classList.add("btn-filtre")
+            const filter = document.createElement("button");
+            filter.innerText = category.name;
+            filter.id = `categorie-${category.id}`;
+            filter.classList.add("btn-filtre");
             if (category.id === 0) {
-                filter.classList.add("active")
-            } 
-            filters.appendChild(filter)
+                filter.classList.add("active");
+            }
+            filters.appendChild(filter);
             filter.addEventListener("click", filtreTravaux);
         }
-    })
+    });
+
+// Appel de la fonction pour obtenir les photos au chargement de la page
+afficherTravaux();
 
 /* Récupération du token */
 const token = localStorage.getItem("token");
 
-/* Affichage du lien logout uniquement si je suis connecté + déconnexion */
-function LogOut() {
-    if (token) {
-        const log = document.querySelector(".log");
-
-        if(log) {
-            log.textContent = "logout";
-            log.removeAttribute("href")
-            log.addEventListener("click", function () {
-                localStorage.removeItem("token");
-                location.reload();
-            })
-        }
-    }
-}
-LogOut ()
-
 /* Affichage du bandeau noir, bouton modifier et disparition du filtre quand je suis connecté */
 function modeEdition() {
     if (token) {
-        const filtresRemove = document.querySelectorAll(".btn-filtre");
+        const filtresRemove = document.getElementById("filtres");
         const boutonModifier = document.querySelector(".btn-modifier-remove");
         const bandeauNoir = document.querySelector(".bandeau-noir-remove");
 
-        filtresRemove.forEach(btn => {
-            btn.classList.replace("btn-filtre", "btn-filtre-remove");
-        });
+        if(filtresRemove) {
+            filtresRemove.id = "filtres-remove";
+        }
 
         if(boutonModifier) {
             boutonModifier.classList.replace("btn-modifier-remove", "btn-modifier")
@@ -102,21 +107,23 @@ let modal = null
 
 const openModal = function (e) {
     e.preventDefault()
-    const target = document.querySelector(e.target.getAttribute("href"))
+    const target = document.querySelector(e.target.getAttribute('href'))
     target.style.display = null
-    target.removeAttribute("aria-hidden")
+    target.removeAttribute('aria-hidden')
     modal = target
-    modal.addEventListener("click", closeModal)
-    modal.querySelector(".fermer-modale-js").addEventListener("click", closeModal)
+    modal.addEventListener('click', closeModal)
+    modal.querySelector('.fermer-modale-js').addEventListener('click', closeModal)
+    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation)
 }
 
 const closeModal = function (e) {
     if (modal === null) return
     e.preventDefault()
     modal.style.display = "none"
-    modal.setAttribute("aria-hidden", "true")
-    modal.removeEventListener("click", closeModal)
-    modal.querySelector(".fermer-modale-js").removeEventListener("click", closeModal)
+    modal.setAttribute('aria-hidden', 'true')
+    modal.removeEventListener('click', closeModal)
+    modal.querySelector('.fermer-modale-js').removeEventListener('click', closeModal)
+    modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation)
     modal = null
 }
 
@@ -124,9 +131,52 @@ const stopPropagation = function (e) {
     e.stopPropagation()
 }
 
-document.querySelectorAll(".modale-js").forEach(a => {
-    a.addEventListener("click", openModal)
+document.querySelectorAll('.modale-js').forEach(a => {
+    a.addEventListener('click', openModal)
 })
 
+/* Affichage de la galerie dans la modale */
+async function afficherTravauxModale() {
+    try {
+        const response = await fetch('http://localhost:5678/api/works');
+        const data = await response.json();
+        const gallery = document.getElementById('gallery-modale');
 
+        data.forEach(photo => {
+            const container = document.createElement('div');
+            container.classList.add('photo-container');
+            container.dataset.category = photo.categoryId;
+
+            const img = document.createElement('img');
+            img.src = photo.imageUrl;
+            img.alt = photo.title;
+            img.classList.add('active');
+
+            container.appendChild(img);
+
+            gallery.appendChild(container);
+        });
+    } catch (error) {
+        console.error('Erreur lors du chargement des photos', error);
+    }
+}
+
+afficherTravauxModale ()
+
+/* Affichage du lien logout uniquement si je suis connecté + déconnexion */
+function LogOut() {
+    if (token) {
+        const log = document.querySelector(".log");
+
+        if(log) {
+            log.textContent = "logout";
+            log.removeAttribute("href")
+            log.addEventListener("click", function () {
+                localStorage.removeItem("token");
+                location.reload();
+            })
+        }
+    }
+}
+LogOut ()
 
